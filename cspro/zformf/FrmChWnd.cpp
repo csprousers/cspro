9b1229d94537457fc09767fd78d5e978d6ad5a90
Orxyamus <greg@csprousers.org>
2025-04-08 17:50:49 -0400

    fixed a Designer crash when using Ctrl+Shift+L when the document was a .fmf, not .ent, file

--- cspro/zformf/FrmChWnd.cpp
+++ cspro/zformf/FrmChWnd.cpp
@@ -2234,18 +2234,25 @@ void CFormChildWnd::OnOptionsFieldProperties()
 }
 
 
-LRESULT CFormChildWnd::OnSwitchView(WPARAM wParam, LPARAM /*lParam*/)
+LRESULT CFormChildWnd::OnSwitchView(const WPARAM wParam, LPARAM /*lParam*/)
 {
-    ViewType view_type = (ViewType)wParam;
+    const ViewType view_type = static_cast<ViewType>(wParam);
 
     if( view_type == ViewType::Form )
+    {
         OnViewForm();
+    }
 
     else if( view_type == ViewType::Logic )
-        OnViewLogic();
+    {
+        if( m_bAppAssociated )
+            OnViewLogic();
+    }
 
     else
+    {
         ASSERT(false);
+    }
 
     return 0;
 }
