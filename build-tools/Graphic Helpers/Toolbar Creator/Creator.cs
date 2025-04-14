ab8e9c08720c025fd9ec34f50eb78fe4bf0808b6
Orxyamus <greg@csprousers.org>
2025-04-14 19:11:00 -0400

    modified the Toolbar Creator build tool so that SVG files are accepted as inputs and are drawn to appropriate size

--- build-tools/Graphic Helpers/Toolbar Creator/Creator.cs
+++ build-tools/Graphic Helpers/Toolbar Creator/Creator.cs
@@ -5,26 +5,31 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using Svg;
 
 namespace ToolbarCreator
 {
     public class Creator
     {
-        private string _inputFilename;
-        private string _outputFilename;
+        private string _inputFilePath;
+        private string _outputFilePath;
+        private int _buttonWidth;
+        private int _buttonHeight;
         private bool _pngMode;
 
-        public Creator(string inputFilename, string outputFilename = null)
+        public Creator(string inputFilePath, string outputFilePath = null, int? buttonWidth = null)
         {
-            _inputFilename = inputFilename;
-            _outputFilename = ( outputFilename != null) ? outputFilename : GetOutputFilename(_inputFilename);
-            _pngMode = ( Path.GetExtension(_outputFilename).ToLower() == ".png" );
+            _inputFilePath = inputFilePath;
+            _outputFilePath = ( outputFilePath != null) ? outputFilePath : GetOutputFilePath(_inputFilePath);
+            _buttonWidth = buttonWidth.GetValueOrDefault(16);
+            _buttonHeight = _buttonWidth - 1;
+            _pngMode = ( Path.GetExtension(_outputFilePath).ToLower() == ".png" );
         }
 
 
-        public static string GetOutputFilename(string inputFilename)
+        public static string GetOutputFilePath(string inputFilePath)
         {
-            return Path.GetFullPath(Path.Combine(Path.GetDirectoryName(inputFilename), Path.GetFileNameWithoutExtension(inputFilename) + ".bmp"));
+            return Path.GetFullPath(Path.Combine(Path.GetDirectoryName(inputFilePath), Path.GetFileNameWithoutExtension(inputFilePath) + ".bmp"));
         }
 
 
@@ -35,18 +40,18 @@ namespace ToolbarCreator
             if( _pngMode )
             {
                 Bitmap toolbar_bitmap = CreateToolbarPng(images);
-                toolbar_bitmap.Save(_outputFilename, ImageFormat.Png);
+                toolbar_bitmap.Save(_outputFilePath, ImageFormat.Png);
             }
 
             else
             {
                 Bitmap toolbar_bitmap = CreateToolbarBmp(images);
-                toolbar_bitmap.Save(_outputFilename, ImageFormat.Bmp);
+                toolbar_bitmap.Save(_outputFilePath, ImageFormat.Bmp);
             }
 
             // open the resultant bitmap in Paint
             if( open_in_paint )
-                Process.Start("mspaint.exe", $"\"{_outputFilename}\"");
+                Process.Start("mspaint.exe", $"\"{_outputFilePath}\"");
         }
 
 
@@ -54,9 +59,9 @@ namespace ToolbarCreator
         {
             List<Image> images = new List<Image>();
 
-            var source_directory = new DirectoryInfo(Path.GetDirectoryName(_inputFilename));
-            
-            foreach( string input_filename in File.ReadAllLines(_inputFilename) )
+            var source_directory = new DirectoryInfo(Path.GetDirectoryName(_inputFilePath));
+
+            foreach( string input_filename in File.ReadAllLines(_inputFilePath) )
             {
                 var files = source_directory.GetFiles(input_filename, SearchOption.AllDirectories);
 
@@ -70,42 +75,150 @@ namespace ToolbarCreator
                     throw new Exception($"Too many {input_filename}");
                 }
 
-                images.Add(Image.FromFile(files[0].FullName));
+                if( files[0].Extension.ToLower().Equals(".svg") )
+                {
+                    images.Add(ConvertSvgToBitmap(files[0].FullName));
+                }
+
+                else
+                {
+                    images.Add(Image.FromFile(files[0].FullName));
+                }
             }
 
             return images;
         }
 
 
-        private static Bitmap CreateToolbarPng(List<Image> images)
-        {
-            // each toolbar image will end up as 15x15, with one pixel separating each image,
-            // but the source will be 225x225 images
-            const int InputSize = 15 * 15;
-            const int InputWidthWithMargin = 16 * 16;
-            const int OutputSize = 15;
-            const int OutputWidthWithMargin = 16;
+        enum CropType { TopRow, BottomRow, Resize };
 
-            var toolbar_bitmap = new Bitmap(InputWidthWithMargin * images.Count, InputSize);
+        private Bitmap ConvertSvgToBitmap(string file_path)
+        {
+            SvgDocument svg_doc = SvgDocument.Open(file_path);
+
+            if( svg_doc.Width != svg_doc.Height )
+                throw new Exception($"Can't work with a SVG with a width ({svg_doc.Width}) different from its height ({svg_doc.Height}).");
+
+            // draw the SVG in the width of the button
+            Bitmap button_width_image = svg_doc.Draw(_buttonWidth, _buttonWidth);
+
+            // because the height of the button is one less than the width, crop the image as necessary
+            Func<Bitmap, int, bool> IsRowTransparent = (bitmap, row) =>
+            {
+                for( int x = 0; x < bitmap.Width; ++x )
+                {
+                    if( bitmap.GetPixel(x, row).A != 0 )
+                        return false;
+                }
+
+                return true;
+            };
+
+            CropType crop_type = IsRowTransparent(button_width_image, _buttonHeight) ? CropType.BottomRow :
+                                 IsRowTransparent(button_width_image, 0)             ? CropType.TopRow :
+                                                                                       CropType.Resize;
+
+            return _pngMode ? ConvertSvgToBitmapForPng(svg_doc, button_width_image, crop_type) :
+                              ConvertSvgToBitmapForBmp(svg_doc, crop_type);
+        }
+
+
+        private Bitmap ConvertSvgToBitmapForPng(SvgDocument svg_doc, Bitmap button_width_image, CropType crop_type)
+        {
+            Bitmap cropped_image = new Bitmap(_buttonWidth, _buttonHeight);
+
+            using( Graphics graphics = Graphics.FromImage(cropped_image) )
+            {
+                Rectangle source_rect;
+
+                // crop the top row
+                if( crop_type == CropType.TopRow )
+                {
+                    source_rect = new Rectangle(0, 1, _buttonWidth, _buttonHeight);
+                }
+
+                // crop the bottom row
+                else if( crop_type == CropType.BottomRow )
+                {
+                    source_rect = new Rectangle(0, 0, _buttonWidth, _buttonHeight);
+                }
+
+                // or redraw to the destination height
+                else
+                {
+                    button_width_image = svg_doc.Draw(_buttonHeight, _buttonHeight);
+                    source_rect = new Rectangle(0, 0, _buttonHeight, _buttonHeight);
+                }
+
+                graphics.DrawImage(button_width_image, 0, 0, source_rect, GraphicsUnit.Pixel);
+            }
+
+            return cropped_image;
+        }
+
+
+        private static Bitmap ConvertSvgToBitmapForBmp(SvgDocument svg_doc, CropType crop_type)
+        {
+            int eventual_button_dimension = ( crop_type == CropType.Resize ) ? 15 : 16;
+            int dimension_to_draw = eventual_button_dimension * eventual_button_dimension;
+
+            Bitmap source_image = svg_doc.Draw(dimension_to_draw, dimension_to_draw);
+            Rectangle source_rect = new Rectangle(0, 0, dimension_to_draw, dimension_to_draw);
+
+            Bitmap cropped_image = new Bitmap(256, 256);
+
+            using( Graphics graphics = Graphics.FromImage(cropped_image) )
+            {
+                // crop the top row
+                if( crop_type == CropType.TopRow )
+                {
+                    source_rect.Y = eventual_button_dimension;
+                    source_rect.Height -= eventual_button_dimension;
+                }
+
+                // crop the bottom row
+                else if( crop_type == CropType.BottomRow )
+                {
+                    source_rect.Height -= eventual_button_dimension;
+                }
+
+                graphics.DrawImage(source_image, 0, 0, source_rect, GraphicsUnit.Pixel);
+            }
+
+            return cropped_image;
+        }
+
+
+        private Bitmap CreateToolbarPng(List<Image> images)
+        {
+            // this routine has been tested creating 16x16 and 20x20 buttons
+            var toolbar_bitmap = new Bitmap(_buttonWidth * images.Count, _buttonHeight);
 
             using( Graphics graphics = Graphics.FromImage(toolbar_bitmap) )
             {
-                // draw each icon
+                // resize and draw each icon
                 for( int i = 0; i < images.Count; ++i )
                 {
-                    var image = images[i];
+                    Image image = images[i];
 
-                    if( image.Width != InputSize || image.Height != InputSize )
-                        throw new Exception($"Can't work with an image {image.Width}x{image.Height}");
+                    if( image.Width != _buttonWidth || image.Height != _buttonHeight )
+                    {
+                        if( image.Width != image.Height )
+                            throw new Exception($"Can't work with an image with a width ({image.Width}) different from its height ({image.Height}).");
 
-                    graphics.DrawImage(image, i * InputWidthWithMargin, 0, InputSize, InputSize);
+                        image = ResizeImage(image, _buttonHeight, _buttonHeight, ResizeMode.Bilinear);
+                    }
+
+                    Debug.Assert(image.Width == _buttonWidth || image.Width == _buttonHeight);
+                    Debug.Assert(image.Height == _buttonHeight);
+
+                    graphics.DrawImage(image, i * _buttonWidth, 0, image.Width, image.Height);
                 }
             }
 
-            // resize to a ...x15 image
-            return ResizeImage(toolbar_bitmap, OutputWidthWithMargin * images.Count, OutputSize, ResizeMode.Bilinear);
+            return toolbar_bitmap;
         }
-        
+
 
         private static Bitmap CreateToolbarBmp(List<Image> images)
         {
@@ -121,14 +234,14 @@ namespace ToolbarCreator
                 // draw each icon
                 for( int i = 0; i < images.Count; ++i )
                 {
-                    var image = images[i];
+                    Image image = images[i];
 
                     if( image.Width != image.Height )
-                        throw new Exception($"Can't work with an image {image.Width}x{image.Height}");
+                        throw new Exception($"Can't work with an image {image.Width}x{image.Height}.");
 
                     if( image.Width != 256 )
                         image = ResizeImage(image, 256, 256, ResizeMode.Icon);
-                    
+
                     graphics.DrawImage(image, i * 256, 0, 256, 256);
                 }
             }
