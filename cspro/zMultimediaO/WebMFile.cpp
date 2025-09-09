4e7b0c06692771e88b1d6a24d3e717d4bf9e8ed4
Oryxamus <greg@csprousers.org>
2025-09-09 12:02:30 -0400

    added the parsing code from libwebm version 1.0.0.32 and the start of a wrapper class, WebMFile, around its functionality

---
+++ cspro/zMultimediaO/WebMFile.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include "WebMFile.h"
+#include <mkvparser/mkvreader.h>
+
+
+std::unique_ptr<mkvparser::MkvReader> WebMFile::CreateReader(const char* const file_path)
+{
+    auto reader = std::make_unique<mkvparser::MkvReader>();
+
+    if( reader->Open(file_path) == 0 )
+    {
+        return reader;
+    }
+
+    else if( PortableFunctions::FileIsRegular(file_path) )
+    {
+        throw FileIO::Exception::FileOpenError(file_path);
+    }
+
+    else
+    {
+        throw FileIO::Exception::FileNotFound(file_path);
+    }
+}
+
+
+bool WebMFile::IsValidFile(const cs::string_sz file_path)
+{
+    const std::unique_ptr<mkvparser::MkvReader> reader = CreateReader(file_path.c_str());
+    mkvparser::EBMLHeader ebml_header;
+    long long pos = 0;
+
+    return ( ebml_header.Parse(reader.get(), pos) >= 0 &&
+             strcmp(ebml_header.m_docType, "webm") == 0 );
+}
---
+++ cspro/zMultimediaO/WebMFile.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <zMultimediaO/zMultimediaO.h>
+
+namespace mkvparser { class MkvReader; }
+
+
+// --------------------------------------------------------------------------
+// WebMFile
+//
+// This class wraps libwebm functionality for reading WebM files.
+// --------------------------------------------------------------------------
+
+class ZMULTIMEDIAO_API WebMFile
+{
+public:
+    // Returns true if the file exists and is a valid WebM file.
+    // A CSProException is thrown if the file does not exist or cannot be opened.
+    static bool IsValidFile(cs::string_sz file_path);
+
+private:
+    static std::unique_ptr<mkvparser::MkvReader> CreateReader(const char* file_path);
+};
