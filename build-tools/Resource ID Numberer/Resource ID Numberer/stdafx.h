4fdaf0ffc11ad6d1655a7c37f0cec5a02fd4d45f
Oryxamus <greg@csprousers.org>
2025-12-02 15:25:00 -0500

    refactored Resource ID Numberer as a 64-bit application into the build-tools solution

--- build-tools/Resource ID Numberer/Resource ID Numberer/stdafx.h
+++ build-tools/Resource ID Numberer/Resource ID Numberer/stdafx.h
@@ -1,8 +1,8 @@
-#pragma once
+﻿#pragma once
 
-#define WIN_DESKTOP
 #include <engine/StandardSystemIncludes.h>
 #include <engine/StrictCompilerErrors.h>
+#include <engine/x64_transition_strict.h>
 
 #include <zToolsO/Tools.h>
 #include <zToolsO/Utf8.h>
--- build-tools/Run Resource ID Numberer (on Recently Modified Files).bat
+++ build-tools/Run Resource ID Numberer (on Recently Modified Files).bat
@@ -5,8 +5,5 @@ if exist %msbuild% goto :start
 set msbuild="C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 :start
-%msbuild% "..\cspro\cspro.sln" /p:Configuration=Debug /target:zToolsO
-%msbuild% "..\cspro\cspro.sln" /p:Configuration=Debug /target:zJson
-
-%msbuild% "Resource ID Numberer\Resource ID Numberer.sln" /p:Configuration=Debug
-"Resource ID Numberer\Debug\Resource ID Numberer.exe" "Resource ID Numberer\Resource File IDs.json" /recent
+%msbuild% "build-tools.sln" /p:Configuration=Debug /p:Platform=x64 /t:Build /target:"Resource ID Numberer"
+"build\x64\Debug\bin\Resource ID Numberer.exe" "Resource ID Numberer\Resource File IDs.json" /recent
