686365dfe4101f29eaead032b4d871858c72e699
Oryxamus <greg@csprousers.org>
2025-12-09 16:53:16 -0500

    moved Stygitan and Git Helpers into the build-tools solution

--- build-tools/Stygitan/MainForm.cs
+++ build-tools/Stygitan/MainForm.cs
@@ -26,7 +26,7 @@ namespace Stygitan
             string[] command_line_arguments = Environment.GetCommandLineArgs();
 
             _gitDirectory = ( command_line_arguments.Length > 1 ) ? Path.GetFullPath(command_line_arguments[1]) :
-                                                                    Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\..\.git"));
+                                                                    Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\.git"));
 
             if( command_line_arguments.Length > 2 )
                 _comparisonBranchName = command_line_arguments[2];
