bb6d4a570c56101fa886dbdcd64c1142d6252dd8
Oryxamus <greg@csprousers.org>
2026-01-14 13:51:25 -0500

    updated the .rc and AssemblyInfo files to update version number 8.0.1 -> 8.1.0

--- cspro/Excel2CSPro/Properties/AssemblyInfo.cs
+++ cspro/Excel2CSPro/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("CSPro Excel to CSPro")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("U.S. Census Bureau")]
-[assembly: AssemblyProduct("CSPro 8.0")]
+[assembly: AssemblyProduct("CSPro 8.1")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("8.0.1.0")]
-[assembly: AssemblyFileVersion("8.0.1.0")]
+[assembly: AssemblyVersion("8.1.0.0")]
+[assembly: AssemblyFileVersion("8.1.0.0")]
