e54e80ee11ffb572dd47298447643ec9b6bb971f
Oryxamus <greg@csprousers.org>
2026-03-31 15:27:21 -0400

    modified the creation of HISTORY.md so that tags corresponding to prereleases are considered "current development"

--- build-tools/Open Source Syncer/ReleaseTag.cpp
+++ build-tools/Open Source Syncer/ReleaseTag.cpp
@@ -7,7 +7,7 @@ std::vector<ReleaseTag> ReleaseTag::Populate(GitRepository& repo, const std::str
 {
     std::vector<ReleaseTag> release_tags;
 
-    std::regex tag_regex(R"(^refs/tags/v(\d+\.\d+\.\d+).*$)");
+    std::regex tag_regex(R"(^refs\/tags\/v(\d+\.\d+\.\d+).*$)");
     std::smatch matches;
 
     repo.ForeachTag(
@@ -23,7 +23,12 @@ std::vector<ReleaseTag> ReleaseTag::Populate(GitRepository& repo, const std::str
             if( version < first_version_sv )
                 return keep_processing;
 
+            std::string tag_name = tag.GetDisplayName();
             std::string tag_message = tag.GetMessage(repo);
+
+            const bool prerelease = ( tag_name.find("-alpha") != std::string::npos ||
+                                      tag_name.find("-beta") != std::string::npos ||
+                                      tag_name.find("-rc") != std::string::npos );
 
             GitCommit commit = repo.LookupCommit(tag);
 
@@ -35,7 +40,7 @@ std::vector<ReleaseTag> ReleaseTag::Populate(GitRepository& repo, const std::str
             if( lookup == release_tags.end() )
             {
                 release_tags.emplace_back(
-                    ReleaseTag { tag.GetDisplayName(), std::move(tag_message), std::move(version), std::move(commit) }
+                    ReleaseTag { std::move(tag_name), std::move(tag_message), std::move(version), prerelease, std::move(commit) }
                 );
             }
 
@@ -44,18 +49,34 @@ std::vector<ReleaseTag> ReleaseTag::Populate(GitRepository& repo, const std::str
             else if( lookup->commit.GetAuthor().GetWhen() < commit.GetAuthor().GetWhen() )
             {
                 ASSERT(combine_releases);
-                lookup->tag_name = tag.GetDisplayName();
+                lookup->tag_name = std::move(tag_name);
                 lookup->tag_message = std::move(tag_message);
+                lookup->prerelease = prerelease;
                 lookup->commit = std::move(commit);
             }
 
             return keep_processing;
         });
 
-
     // sort by version
     std::sort(release_tags.begin(), release_tags.end(),
               [&](const ReleaseTag& rt1, const ReleaseTag& rt2) { return ( rt1.tag_name < rt2.tag_name ); });
+
+    // validate the tags
+    const size_t prerelease_tags =
+        std::count_if(release_tags.cbegin(), release_tags.cend(),
+                      [&](const ReleaseTag& rt) { return rt.prerelease; });
+
+    if( prerelease_tags > 1 )
+    {
+        throw CSProException("There should not be %zu prerelease tags.", prerelease_tags);
+    }
+
+    else if( prerelease_tags == 1 &&
+             !release_tags.back().prerelease )
+    {
+        throw CSProException("The only prerelease tag must be the final one: " + release_tags.back().tag_name);
+    }
 
     return release_tags;
 }
