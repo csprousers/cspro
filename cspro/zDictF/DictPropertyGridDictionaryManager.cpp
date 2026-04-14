7fed7185b894e5138696bb9ced09679e405d667b
Oryxamus <greg@csprousers.org>
2026-04-14 14:34:10 -0400

    moved the "Syncable Name Override" property panel property under the Advanced header

--- cspro/zDictF/DictPropertyGridDictionaryManager.cpp
+++ cspro/zDictF/DictPropertyGridDictionaryManager.cpp
@@ -1,8 +1,8 @@
-﻿#include "StdAfx.h"
+#include "StdAfx.h"
 #include "DictPropertyGridDictionaryManager.h"
 
 
-DictPropertyGridDictionaryManager::DictPropertyGridDictionaryManager(CDDDoc* pDDDoc, CDataDict& dictionary)
+DictPropertyGridDictionaryManager::DictPropertyGridDictionaryManager(CDDDoc* const pDDDoc, CDataDict& dictionary)
     :   DictPropertyGridBaseManager(pDDDoc, dictionary, L"Dictionary"),
         m_dictionary(dictionary)
 {
@@ -19,15 +19,32 @@ void DictPropertyGridDictionaryManager::SetupProperties(CMFCPropertyGridCtrl& pr
 {
     AddGeneralSection<CDataDict>(property_grid_ctrl);
 
-    // Synchronization heading
-    auto synchronization_heading_property = new PropertyGrid::HeadingProperty(L"Synchronization");
-    property_grid_ctrl.AddProperty(synchronization_heading_property);
+    // Advanced heading
+    auto advanced_heading_property = new PropertyGrid::HeadingProperty(L"Advanced");
+    property_grid_ctrl.AddProperty(advanced_heading_property);
+
+
+    // Read Optimization property
+    advanced_heading_property->AddSubItem(
+        PropertyGrid::PropertyBuilder<bool>(
+            L"Read Optimization",
+            L"If enabled, only items used in logic are read, resulting in the faster reading of data files.",
+            m_dictionary.GetReadOptimization()
+        )
+        .SetOnUpdate([&](const bool& read_optimization)
+            {
+                m_dictionary.SetReadOptimization(read_optimization);
+            })
+        .Create());
+
 
     // Syncable Name Override property
-    synchronization_heading_property->AddSubItem(
-        PropertyGrid::PropertyBuilder<std::string>(L"Syncable Name Override",
-                                                   L"When defined, data will be sent to synchronization services using the provided name rather than the dictionary name.",
-                                                   m_dictionary.GetSyncableName(false))
+    advanced_heading_property->AddSubItem(
+        PropertyGrid::PropertyBuilder<std::string>(
+            L"Syncable Name Override",
+            L"When defined, data will be sent to synchronization services using the provided name rather than the dictionary name.",
+            m_dictionary.GetSyncableName(false)
+        )
         .SetOnFormat([&](const std::string& syncable_name)
             {
                 return TC::ToWide<CString>(SO::ToUpper(SO::Trim(syncable_name)));
@@ -38,29 +55,16 @@ void DictPropertyGridDictionaryManager::SetupProperties(CMFCPropertyGridCtrl& pr
 
                 if( !trimmed_syncable_name_sv.empty() && !CIMSAString::IsName(trimmed_syncable_name_sv) )
                 {
-                    throw PropertyGrid::PropertyValidationException(std::string(), "'%s' is not a valid CSPro name",
-                                                                                   std::string(trimmed_syncable_name_sv).c_str());
+                    throw PropertyGrid::PropertyValidationException(
+                        std::string(),
+                        "'%s' is not a valid CSPro name",
+                        std::string(trimmed_syncable_name_sv).c_str()
+                    );
                 }
             })
         .SetOnUpdate([&](const std::string& syncable_name)
             {
                 m_dictionary.SetSyncableName(SO::ToUpper(SO::Trim(syncable_name)));
-            })
-        .Create());
-
-
-    // Advanced heading
-    auto advanced_heading_property = new PropertyGrid::HeadingProperty(L"Advanced");
-    property_grid_ctrl.AddProperty(advanced_heading_property);
-
-    // Read Optimization property
-    advanced_heading_property->AddSubItem(
-        PropertyGrid::PropertyBuilder<bool>(L"Read Optimization",
-                                            L"If enabled, only items used in logic are read, resulting in the faster reading of data files.",
-                                            m_dictionary.GetReadOptimization())
-        .SetOnUpdate([&](const bool& read_optimization)
-            {
-                m_dictionary.SetReadOptimization(read_optimization);
             })
         .Create());
 }
