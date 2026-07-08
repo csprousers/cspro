7a3dfdfe10b409b5992bf880556dafd32848a6cc
Oryxamus <greg@csprousers.org>
2026-07-08 11:24:56 -0400

    fixed Application.getQuestionnaireContent to work when the interpreter is not available
    bug introduced in [273a1bf]

--- cspro/zAction/ActionInvoker.h
+++ cspro/zAction/ActionInvoker.h
@@ -142,8 +142,7 @@ private:
 
 
     // Data
-    static std::unique_ptr<Case> ReadCase(const JsonNode& json_node, DataRepository& data_repository,
-                                          bool return_null_if_no_case_identifier_present);
+    static std::unique_ptr<const Case> ReadCase(const JsonNode& json_node, const char* identifier, DataRepository& data_repository);
 
     Result GetQuestionnaireContentWithCaseData(std::variant<std::shared_ptr<const CDataDict>, std::unique_ptr<QuestionnaireContentCreator>> dictionary_or_questionnaire_content_creator,
                                                std::unique_ptr<const Case> data_case, const JsonNode& json_node,
