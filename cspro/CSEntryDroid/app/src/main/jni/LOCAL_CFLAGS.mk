135a23d5f2ec614c25b3572c8dfb2a37cc6028cf
Oryxamus <greg@csprousers.org>
2026-07-14 12:31:01 -0400

    removed the Android makefiles

--- cspro/CSEntryDroid/app/src/main/jni/LOCAL_CFLAGS.mk
+++
@@ -1,9 +0,0 @@
-
-# building on Android
-LOCAL_CFLAGS += -DANDROID=1
-
-# suppress warning: expression result unused
-LOCAL_CFLAGS += -Wno-unused-value
-
-# suppress warning: enumeration values not handled in switch
-LOCAL_CFLAGS += -Wno-switch
